3e103c6a56c7ae831fa1fed7bca697c964d2b328
Кулицкий <@ASU-2.ZSK.BY>
2013-09-03 02:23:12 +1200

    second

---
+++ VALIDATION/src/main/java/db/load_classicmodels.sql
@@ -0,0 +1,84 @@
+/******************************************************************************
+ * Copyright (c) 2005 Actuate Corporation.
+ * All rights reserved. This file and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Actuate Corporation  - initial implementation
+ *
+ * Classic Models Inc. sample database developed as part of the
+ * Eclipse BIRT Project. For more information, see http:\\www.eclipse.org\birt
+ *
+ *******************************************************************************/
+
+/* Loads the Classic Models tables using the MySQL LOAD command */
+
+/* Preparing the load files for importing. Input file requirements:
+     - Column order in the file must be the same as the columns in the table
+     - Columns are Comma delimited
+     - Text is quoted (")
+     - NULL columns must be ,NULL,  ( ,, is not acceptable)
+     - Dates must be in YYYY-MM-DDD format
+
+   Input files expected in the datafiles direcory, parallel to this script.
+*/
+
+
+/* First make sure all the tables are empty */
+
+DELETE FROM Customers;
+DELETE FROM Employees;
+DELETE FROM Offices;
+DELETE FROM OrderDetails;
+DELETE FROM Orders;
+DELETE FROM Payments;
+DELETE FROM Products;
+DELETE FROM ProductLines;
+
+# Load records into the tables. There should be no warnings.
+
+SET @path='D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt';
+
+LOAD DATA LOCAL INFILE 'D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt' INTO TABLE Customers
+          FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
+
+SHOW WARNINGS LIMIT 10;
+
+LOAD DATA LOCAL INFILE 'D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt' INTO TABLE Employees
+          FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
+
+SHOW WARNINGS LIMIT 10;
+
+LOAD DATA LOCAL INFILE 'D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt' INTO TABLE Offices
+          FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
+
+SHOW WARNINGS LIMIT 10;
+
+LOAD DATA LOCAL INFILE 'D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt' INTO TABLE OrderDetails
+          FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
+
+SHOW WARNINGS LIMIT 10;
+
+LOAD DATA LOCAL INFILE 'D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt' INTO TABLE Orders
+          FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
+
+SHOW WARNINGS LIMIT 10;
+
+LOAD DATA LOCAL INFILE 'D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt' INTO TABLE Payments
+          FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
+
+SHOW WARNINGS LIMIT 10;
+
+LOAD DATA LOCAL INFILE 'D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt' INTO TABLE Products
+          FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n';
+
+SHOW WARNINGS LIMIT 10;
+
+LOAD DATA LOCAL INFILE 'D:\\projects\\solr\\src\\main\\resources\\datafiles\\customers.txt' INTO TABLE ProductLines 
+          FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' 
+          (productLine , textDescription);
+
+SHOW WARNINGS LIMIT 10;
+
